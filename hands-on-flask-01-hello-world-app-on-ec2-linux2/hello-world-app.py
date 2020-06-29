8f597ddfd73a272abf7118f70c56fe15829d4f4c
Betul <betul.nalan@gmail.com>
2020-06-29 17:24:33 -0400

    hello-world-app.py

--- hands-on-flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
+++ hands-on-flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
@@ -4,9 +4,11 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello world"
+    for i in range(3):
+
+        return "<h2>Hello world, again me</h2><h1><i>Betul</i></h1"
 
 if __name__=='__main__':
-    #app.run('localhost', port=5000, debug=True) for run internally
-    #app.run(debug=True) -->same with last one
-    app.run('0.0.0.0', port=80) #for run on internet
+    #app.run('localhost', port=5000, debug=True) 
+    #app.run(debug=True) 
+    app.run('0.0.0.0', port=80) 
